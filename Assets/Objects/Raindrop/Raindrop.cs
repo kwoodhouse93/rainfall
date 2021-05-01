776163420c8cd65eab6cf8a69a16754caa9cfe1e
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2021-05-01 08:35:46 +0100

    Add random chance to spawn/despawn raindrops
    So we don't always have the same number of drops
    
    Going to need some tuning to make sure we don't always converge on 0 or
    blow up to infinite raindrops

--- Assets/Objects/Raindrop/Raindrop.cs
+++ Assets/Objects/Raindrop/Raindrop.cs
@@ -1,11 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Raindrop : MonoBehaviour
 {
+    // Object to spawn when collision is processed - perhaps a particle system
+    // for a shatter or splash effect?
     [SerializeField] private GameObject spawnOnDestroy;
+    // Allows raindrops to survive for a random amount of time between 0 and
+    // maxCollisionLife after hitting a trigger - cheaply simulates a more
+    // 3d body of water - longer living raindrops appear to be closer to the
+    // camera.
     [SerializeField] private float maxCollisionLife;
+    // How likely collision is to despawn the object (0 to 1).
+    [SerializeField] private float destructionChance;
 
     private Rainfall rainfall;
     private TrailRenderer trailRenderer;
@@ -26,6 +34,12 @@ public class Raindrop : MonoBehaviour
         yield return new WaitForSeconds(Random.Range(0, maxCollisionLife));
 
         Object.Instantiate(spawnOnDestroy, transform.position, transform.rotation);
+
+        if (Random.Range(0f, 1f) < destructionChance)
+        {
+            Debug.Log("Destroying");
+            Object.Destroy(gameObject);
+        }
 
         transform.position = rainfall.NewSpawnPosition();
         trailRenderer.Clear();
--- Assets/Objects/Rainfall.cs
+++ Assets/Objects/Rainfall.cs
@@ -1,14 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rainfall : MonoBehaviour
 {
+    [SerializeField] private Raindrop raindrop;
+
+    [Header("Initial spawn parameters")]
     [SerializeField] private float spawnHeight;
     [SerializeField] private float spawnCount;
     [SerializeField] private float spawnRate;
     [SerializeField] private float spawnRange;
-    [SerializeField] private Raindrop raindrop;
+
+    [Header("Subsequent spawn parameters")]
+    [SerializeField] private float spawnChance;
+
+    [Header("Wobble parameters")]
     [SerializeField] private float wobbleFreq;
     [SerializeField] private float wobbleAmount;
     [SerializeField] private float wobbleRandomness;
@@ -17,17 +23,34 @@ public class Rainfall : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(SpawnRaindrops());
+        StartCoroutine(SpawnInitialRaindrops());
     }
 
     void Update()
     {
+        // Rotate the entire parent object to get a wavy motion. Just a touch
+        // of randomness gives it a shaky effect.
         float angle = Mathf.Sin(Time.time * wobbleFreq + Random.Range(-wobbleRandomness, wobbleRandomness)) * wobbleAmount;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Random chance to spawn a new raindrop at each spawn interval after the initial spawns.
+        if (Time.time > nextSpawn)
+        {
+            Debug.Log("Testing spawn");
+            if (Random.Range(0f, 1f) < spawnChance)
+            {
+                Debug.Log("Spawning");
+                Object.Instantiate(raindrop, NewSpawnPosition(), Quaternion.identity, transform);
+            }
+
+            nextSpawn = Time.time + spawnRate;
+        }
     }
 
-    private IEnumerator SpawnRaindrops()
+    private IEnumerator SpawnInitialRaindrops()
     {
+        // Make sure we don't spawn multiple raindrops during the setup coroutine
+        nextSpawn = Time.time + (spawnRate * (spawnCount + 1));
         for (int i = 0; i < spawnCount; i++)
         {
             Object.Instantiate(raindrop, NewSpawnPosition(), Quaternion.identity, transform);
